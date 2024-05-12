f699bcf4e9357af939154546d0fb599cd01ff87c
Pranshu-Shakya <pranshushakya12b20@gmail.com>
2024-05-12 14:50:33 +0530

    Create 266A.java

---
+++ 266A.java
@@ -0,0 +1,15 @@
+// https://codeforces.com/problemset/problem/266/A
+import java.util.Scanner;
+public class Stones {
+    public static void main(String args[]) {
+      Scanner sc = new Scanner(System.in);
+      int n = sc.nextInt();
+      String s = sc.next();
+      int count = 0;
+      for(int i = 1; i<s.length(); i++) {
+          if (s.charAt(i-1) == s.charAt(i))
+          count++;
+      }
+      System.out.println(count);
+    }
+}

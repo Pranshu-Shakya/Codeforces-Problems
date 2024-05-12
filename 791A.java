b835b7f8062e6448e5bfcb13021f0cbe28ce6545
Pranshu-Shakya <pranshushakya12b20@gmail.com>
2024-05-12 14:31:45 +0530

    Create 791A.java

---
+++ 791A.java
@@ -0,0 +1,16 @@
+// https://codeforces.com/problemset/problem/791/A
+import java.util.Scanner;
+public class MyClass {
+    public static void main(String args[]) {
+      Scanner sc = new Scanner(System.in);
+      int a = sc.nextInt();
+      int b = sc.nextInt();
+      int count = 0;
+      while (a<=b) {
+          a*=3;
+          b*=2;
+          count++;
+      }
+      System.out.println(count);
+    }
+}
